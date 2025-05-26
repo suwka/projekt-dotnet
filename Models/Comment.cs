903451808c47d2865d75a0354512518d2813dbbe
krzysztof leczycki <71281946+suwka@users.noreply.github.com>
2025-05-27 01:45:37 +0200

    comment - komentarze do zlecen serwisowych

---
+++ Models/Comment.cs
@@ -0,0 +1,28 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace WorkshopManager.Models
+{
+    public class Comment
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string AuthorId { get; set; }
+        public IdentityUser Author { get; set; }
+
+        [Required]
+        [StringLength(1000)]
+        public string Content { get; set; }
+
+        [Required]
+        public DateTime Timestamp { get; set; }
+
+        [Required]
+        public int ServiceOrderId { get; set; }
+        public ServiceOrder ServiceOrder { get; set; }
+    }
+}
+
