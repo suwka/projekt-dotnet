53cdd439c21bd0d014d613f985c289b21654010e
Cinkut <167883583+Cinkut@users.noreply.github.com>
2025-05-27 13:48:48 +0200

    Konfiguracja Identity z obsługą ról oraz automatycznym seedowaniem ról i konta administratora

---
+++ Data/DbSeeder.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+
+namespace WorkshopManager.Data
+{
+    public static class DbSeeder
+    {
+        public static async Task SeedRolesAndAdminAsync(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+            string[] roleNames = { "Admin", "Mechanik", "Recepcjonista", "Klient" };
+            foreach (var roleName in roleNames)
+            {
+                if (!await roleManager.RoleExistsAsync(roleName))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(roleName));
+                }
+            }
+
+            // Tworzenie konta administratora
+            string adminEmail = "admin@warsztat.pl";
+            string adminPassword = "Admin123!";
+            if (await userManager.FindByEmailAsync(adminEmail) == null)
+            {
+                var adminUser = new IdentityUser { UserName = adminEmail, Email = adminEmail, EmailConfirmed = true };
+                var result = await userManager.CreateAsync(adminUser, adminPassword);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(adminUser, "Admin");
+                }
+            }
+        }
+    }
+}
+
