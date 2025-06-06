160748c970cb7b5c3d759ac1da3d7731520e9dfe
Cinkut <167883583+Cinkut@users.noreply.github.com>
2025-06-06 12:00:30 +0200

    Implementacja funkcjonalności edycji komentarzy dla zleceń serwisowych
    - Dodano pole ModifiedAt do modelu Comment przechowujące datę edycji komentarza
    - Zaktualizowano metodę EditComment w kontrolerze do ustawiania daty modyfikacji
    - Utworzono widok EditComment.cshtml z formularzem do edycji komentarzy
    - Dodano wyświetlanie informacji o dacie edycji komentarzy w szczegółach zlecenia
    - Utworzono migrację AddModifiedAtToComment dla nowej kolumny w bazie danych
    
    Teraz recepcjoniści, mechanicy i klienci mogą edytować swoje komentarze,
    a system wyświetla informację o dacie i czasie edycji obok oryginalnej daty dodania komentarza.

--- Models/Comment.cs
+++ Models/Comment.cs
@@ -19,6 +19,8 @@ namespace WorkshopManager.Models
 
         [Required]
         public DateTime Timestamp { get; set; }
+        
+        public DateTime? ModifiedAt { get; set; }
 
         [Required]
         public int ServiceOrderId { get; set; }
