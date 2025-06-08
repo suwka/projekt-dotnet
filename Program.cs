819f2d25abbf41e2d0587ece94e7e26b31056f6f
Cinkut <167883583+Cinkut@users.noreply.github.com>
2025-06-08 17:57:53 +0200

    elo

---
+++ Program.cs
@@ -0,0 +1,69 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using WorkshopManager.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<WorkshopManager.Data.ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Rejestracja własnych serwisów
+builder.Services.AddScoped<PdfService>();
+builder.Services.AddScoped<EmailService>();
+
+// Rejestracja usługi tła do generowania raportów
+builder.Services.AddHostedService<OpenOrderReportBackgroundService>();
+
+builder.Services.AddIdentity<Microsoft.AspNetCore.Identity.IdentityUser, Microsoft.AspNetCore.Identity.IdentityRole>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = false; // dla testów
+})
+.AddEntityFrameworkStores<WorkshopManager.Data.ApplicationDbContext>()
+.AddDefaultTokenProviders();
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/Identity/Account/Login";
+    options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+});
+
+var app = builder.Build();
+
+// Seeder wywołanie
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        WorkshopManager.Data.DbSeeder.SeedRolesAndAdminAsync(services).GetAwaiter().GetResult();
+        Console.WriteLine("Seeder: Role i admin utworzone lub już istnieją.");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Seeder error: {ex.Message}");
+    }
+}
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
